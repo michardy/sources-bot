76302c651dcf1d00b45276b7e32c7e89c4c4a9d0
Michael Hardy <98.michael.hardy@gmail.com>
2018-05-19 13:50:00 -0700

    Actually support all syntax rules

--- syntax.py
+++ syntax.py
@@ -12,12 +12,19 @@ class Syntax():
 			person: 1st person, 2nd person, 3rd person (note: 3rd person is stored 'THIRD')
 	'''
 	def __init__(self, token, pos):
-		self.offset = pos
-		self.content = token.text.content
-		self.simplified = token.lemma
-		self.dependency = token.dependency_edge.label
-		self.tag = token.part_of_speech.tag
-		self.number = token.part_of_speech.number
-		self.case = token.part_of_speech.case
-		self.gender = token.part_of_speech.gender
-		self.person = token.part_of_speech.person
+		self.offset      = pos
+		self.content     = token.text.content
+		self.simplified  = token.lemma
+		self.dependency  = token.dependency_edge.label
+		self.tag         = token.part_of_speech.tag
+		self.number      = token.part_of_speech.number
+		self.case        = token.part_of_speech.case
+		self.gender      = token.part_of_speech.gender
+		self.person      = token.part_of_speech.person
+		self.aspect      = token.part_of_speech.aspect
+		self.form        = token.part_of_speech.form
+		self.mood        = token.part_of_speech.mood
+		self.proper      = token.part_of_speech.proper
+		self.reciprocity = token.part_of_speech.reciprocity
+        self.tense       = token.part_of_speech.tense
+        self.voice       = token.part_of_speech.voice
