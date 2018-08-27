e4c41c041ae8e492ba0d9d45f6db7e7cf5b069f4
Michael Hardy <98.michael.hardy@gmail.com>
2018-08-27 00:08:50 -0700

    More work on cron job

--- langtree.py
+++ langtree.py
@@ -1,8 +1,27 @@
 from syntax import Syntax
+from google.cloud.language import enums
 
 class Langtree():
 	'''Fully annotated tree representing sentence structure.
 	Contains both entities and syntax.'''
+	def __get_true_children(self, child, parent, tokens, processed, ref):
+		'''Checks that the next nodes are not just chained nouns
+		If they are chained then add the chained contents and link the next element'''
+		if token.dependency_edge.label == enums.DependencyEdge.Label.NN or
+			token.dependency_edge.label == enums.DependencyEdge.Label.APPOS:
+			assert(parent != child, 'Self referential non root node')
+			if parent < child:
+				self.syntax.content += ' ' + tokens[child].text.content
+				self.syntax.simplified += ' ' + tokens[child].lemma
+			else:
+				self.syntax.content = tokens[t].text.content + ' ' +
+					self.syntax.content
+				self.syntax.simplified = tokens[t].lemma + ' ' +
+					self.syntax.simplified
+			return(self.__branch(child, tokens, processed, ref))
+		else:
+			return(Langtree(child, tokens, processed, ref))
+
 	def __branch(self, parent, tokens, processed, ref):
 		'''Adds a branch to the tree'''
 		t = 0 # iterator for scanning flattened tree
@@ -10,7 +29,7 @@ class Langtree():
 			if not processed[t]:
 				token = tokens[t]
 				if token.dependency_edge.head_token_index == parent:
-					return(Langtree(t, tokens, processed, ref))
+					self.__get_true_children(t, parent, tokens, processed, ref)
 			if t < len(processed) - 1:
 				t += 1
 			else:
@@ -34,6 +53,72 @@ class Langtree():
 				break
 			else:
 				self.branches.append(new_branch)
+
+	def __list_add(l, item):
+		'''Set like list add function. Allows us to use JSON serializable lists'''
+		if item not in l:
+			l.add(item)
+
+	def get_simple_parts(self, simplified, dependency):
+		'''Function to return sentence components of a subset of the sentence.
+		Goes from the root element to the first adapositional clause
+		'''
+		if self.syntax.dependency == enums.PartOfSpeech.Tag.ADP:
+			return(simplified)
+		elif self.syntax.dependency == enums.DependencyEdge.Label.ROOT:
+			simplified['action'] = self.syntax.simplified
+		elif self.syntax.dependency == enums.DependencyEdge.Label.DOBJ:
+			if self.entity is not None:
+				simplified['target_entity'] = self.entity.id
+			simplified['target'] = self.syntax.lemma
+		elif self.syntax.dependency == enums.DependencyEdge.Label.NSUBJ:
+			if self.entity is not None:
+				simplified['actor_entity'] = self.entity.id
+			simplified['actor'] = self.syntax.lemma
+		elif self.syntax.dependency == enums.DependencyEdge.Label.POSS:
+			if dependency == enums.DependencyEdge.Label.DOBJ:
+				if self.entity is not None:
+					simplified['target_owner_entity'] = self.entity.id
+				simplified['target_owner'] = self.syntax.lemma
+			elif dependency == enums.DependencyEdge.Label.NSUBJ:
+				if self.entity is not None:
+					simplified['actor_owner_entity'] = self.entity.id
+				simplified['actor_owner'] = self.syntax.lemma
+		for branch in self.branches:
+			simplified = branch.get_simple_parts(simplified, self.syntax.dependency)
+		return(simplified)
+
+	def get_characteristics(self, r):
+		'''Get people, places, organizations, actions and other things of signifigance'''
+		if self.entity is not None:
+			if self.entity.type == enums.Entity.Type.LOCATION:
+				if self.syntax.simplified not in r['places']:
+					self.__list_add(r['places'], self.syntax.simplified)
+					if self.entity.id is not None:
+						self.__list_add(r['place_ids'], self.entity.id)
+			elif self.entity.type == enums.Entity.Type.ORGANIZATION:
+				if self.syntax.simplified not in r['organizations']:
+					self.__list_add(r['organizations'], self.syntax.simplified)
+					if self.entity.id is not None:
+						self.__list_add(r['orgaization_ids'], self.entity.id)
+			elif self.entity.type == enums.Entity.Type.PERSON:
+				if self.syntax.simplified not in r['people']:
+					self.__list_add(r['people'], self.syntax.simplified)
+					if self.entity.id is not None:
+						self.__list_add(r['person_ids'], self.entity.id)
+		else:
+			if self.syntax.tag == enums.PartOfSpeech.Tag.NOUN:
+				if self.syntax.simplified not in r['things']:
+					self.__list_add(r['things'], self.syntax.simplified)
+			elif self.syntax.tag == enums.PartOfSpeech.Tag.VERB:
+				if self.syntax.simplified not in r['actions']:
+					self.__list_add(r['actions'], self.syntax.simplified)
+			# needs to be rewritten
+			#elif t.syntax.tag == 'PUNCT':
+			#	r = find_mandatory_words_end(t, n, r)
+		for b in self.branches:
+			r = b.get_characteristics(r)
+		return(r)
 
 	def __str__(self):
 		return("{\n" + str(self.syntax) + "\n" + str(self.entity) + "\n" + str(self.branches) + "\n}")
--- update_sources.py
+++ update_sources.py
@@ -2,11 +2,9 @@
 
 import datetime
 import re
-import pickle
-import sys
-import praw
 from bs4 import BeautifulSoup
-from string import Template
+from elasticsearch import Elasticsearch
+from google.cloud.language import enums
 try:
 	import urllib2
 	from urlparse import urljoin
@@ -17,138 +15,56 @@ except ImportError:
 from news import *
 from annotator import Annotator
 
-# Nessisary for caching to work
-# If removed the picking operation will exceed the recursion limit
-sys.setrecursionlimit(100000)
-
-reddit = praw.Reddit('sourcesbot', user_agent='web:sourcesbot:v0.0.2 by /u/michaelh115')
-
 annotator = Annotator()
-
-TEMPLATE = '''Other sources for this story:
-
-$sources
-
-Opinions:
-
-$opinions
-
-__________
-
-^(*Note on opinions: due to sourcing and how articles are matched the opinions may not be terribly diverse.*)
-
-^*[feedback](https://www.reddit.com/r/sourcesbot/comments/6v0pa5/feedback/)* ^| ^*[usage](https://www.reddit.com/r/sourcesbot/wiki/index)* ^| ^*[code]($code)* ^| ^*author:* ^*$writer*'''
-
-TEMPLATE_NFOUND = '''No acceptable matches were found
-
-__________
-
-^*[feedback](https://www.reddit.com/r/sourcesbot/comments/6v0pa5/feedback/)* ^| ^*[usage](https://www.reddit.com/r/sourcesbot/wiki/index)* ^| ^*[code]($code)* ^| ^*author:* ^*$writer*'''
-
-
-
-def find_mandatory_words_start(t, n, r):
-	if n == 0: # start end
-		if len(t) > 2 and len(t[n+1]) == 2:
-			if t[n+1][1] == ':': # <Name>:
-				r = read_words(t[n], 'mandate', 'speakers', r)
-		elif len(t) > 4 and len(t[n+1]) == 2:
-			if (t[n+1][1] == 'CC' and
-				t[n+3].label() == 'PERSON' and
-				t[n+4].label() == ':'): # <Name> and <Name>:
-				r = read_words(t[n], 'mandate', 'speakers', r)
-				r = read_words(t[n+3], 'mandate', 'speakers', r)
-	return(r)
-
-def find_mandatory_words_end(t, n, r):
-	if n == len(t) - 1:
-		if len(t)-n > 2:
-			if t[n+1].label() == 'PERSON': # <Name>:
-				r = read_words(t[n+1], 'mandate', 'speakers', r)
-		elif len(t)-n > 4:
-			if (t[n+1].label() == 'PERSON' and
-				t[n+3][1] == 'CC' and
-				t[n+4].label() == 'PERSON'): # <Name> and <Name>:
-				r = read_words(t[n+1], 'mandate', 'speakers', r)
-				r = read_words(t[n+4], 'mandate', 'speakers', r)
-	return(r)
-
-def get_characteristics_from_list(trees, r):
-	for t in trees:
-		get_characteristics(t, r)
-	return(r)
-
-def get_characteristics(t, r):
-	# Get people, places, organizations, actions and other things of signifigance
-	if t.entity is not None:
-		if t.entity.type == 2:
-			if t.syntax.simplified not in r['talley']['places']:
-				r['talley']['places'].append(t.syntax.simplified)
-		elif t.entity.type == 3:
-			if t.syntax.simplified not in r['talley']['organizations']:
-				r['talley']['organizations'].append(t.syntax.simplified)
-		elif t.entity.type == 1:
-			if t.syntax.simplified not in r['talley']['people']:
-				r['talley']['people'].append(t.syntax.simplified)
-	else:
-		if t.syntax.tag == 6:
-			if t.syntax.simplified not in r['talley']['things']:
-				r['talley']['things'].append(t.syntax.simplified)
-		elif t.syntax.tag == 11:
-			if t.syntax.simplified not in r['talley']['actions']:
-				r['talley']['actions'].append(t.syntax.simplified)
-		# needs to be rewritten
-		#elif t.syntax.tag == 'PUNCT':
-		#	r = find_mandatory_words_end(t, n, r)
-	for b in t.branches:
-		get_characteristics(b, r)
-	return(r)
+es = Elasticsearch()
 
 class Source:
 	'''Generic class describing sources and how to parse them'''
-	def __init__(self):
-		self._time = None
-		self._content = None
-
 	def get(self):
-		'''Gets source content and updates it if it is more than 30 minutes old'''
-		refresh = datetime.timedelta(minutes=30)
-		if datetime.datetime.utcnow() - self._time > refresh:
-			self.update()
-		return(self._content)
-
-	def update(self):
 		'''Generic method to scan home pages.  Must be overriden'''
 		raise NotImplementedError("Must override update")
 
-	def _characterize(self):
-		'''Gets the characteristics of every story on a sources home page'''
-		for s in range(len(self._content)):
-			# The default r NEEDS to be passed or the function below becomes possessed
-			c = get_characteristics_from_list(
-				self._content[s]['machine_title'],
-				{
-					'talley':{
-						'places':[],
-						'people':[],
-						'organizations':[],
-						'things':[],
-						'actions':[]
-					},
-					'mandate':{
-						'speakers':[]
-					}
+	def _process(self, url, title, desc):
+		time = datetime.datetime.utcnow()
+		'''Gets the characteristics of  a story and saves it'''
+		query = {
+			"query": {
+				"match": {
+					"url": url
 				}
-			)
-			c = get_characteristics_from_list(self._content[s]['description'], c)
-			c = dedup_entities(c)
-			self._content[s]['character'] = c
+			}
+		}
+		if es.search(index="stories*", body=query)['hits']['total'] == 0:
+			document = {
+				'url': url,
+				'timestamp': time,
+				'title': title,
+				'description': desc,
+				'people': [],
+				'places': [],
+				'organizations': [],
+				'things': [],
+				'actions': [],
+				'speakers': []
+			}
+			ideas = []
+			machine_title = annotator.annotate(title)
+			for sentence in machine_title:
+				ideas.append(sentence.get_simple_parts({}, None))
+				document = sentence.get_characteristics(document)
+			if desc is not None:
+				machine_desc = annotator.annotate(desc)
+				for sentence in machine_desc:
+					ideas.append(sentence.get_simple_parts({}, None))
+					document = sentence.get_characteristics(document)
+			document['ideas'] = ideas
+			es.index(index='stories*', body=document)
 
 class AlJazeera(Source):
 	def __isolate_content(self, links):
-		self._content = []
 		for h in links:
-			desc, title = '', ''
+			desc = None
+			title = ''
 			try:
 				if str(h.contents[0]).startswith('<'):
 					if not str(h.contents[0].contents[0]).startswith('<'):
@@ -168,33 +84,20 @@ class AlJazeera(Source):
 					url = urljoin('http://www.aljazeera.com/', url)
 			except KeyError: # Yes, here at Al Jazeera we use empty <a> tags!
 				pass
-			machine_title = annotator.annotate(title)
-			thresh = 2
-			if desc:
-				thresh += 1
-			desc = annotator.annotate(desc)
-			self._content.append({
-				'title':title,
-				'machine_title':machine_title,
-				'description':desc,
-				'threshold':thresh,
-				'url':url
-			})
+			self._process(url, title, desc)
 
-	def update(self):
-		self._time = datetime.datetime.utcnow()
+	def get(self):
 		r = urllib2.urlopen("http://www.aljazeera.com/")
 		html = r.read()
 		soup = BeautifulSoup(html, "html.parser")
 		links = soup.find_all('a')
 		self.__isolate_content(links)
-		self._characterize()
 
 class Bbc(Source):
 	def __isolate_content(self, stories):
 		self._content = []
 		for s in stories:
-			desc = ''
+			desc = None
 			h = s.find_all('a', {'class':'gs-c-promo-heading'})
 			if len(h) > 0:
 				if not str(h[0].contents[0].contents[0]).startswith('<'):
@@ -207,18 +110,7 @@ class Bbc(Source):
 				url = h[0]['href']
 				if url.startswith('/'):
 					url = urljoin('http://www.bbc.com/news', url)
-				machine_title = annotator.annotate(title)
-				thresh = 2
-				if desc:
-					thresh += 1
-				desc = annotator.annotate(desc)
-				self._content.append({
-					'title':title,
-					'machine_title':machine_title,
-					'description':desc,
-					'threshold':thresh,
-					'url':url
-				})
+				self._process(url, title, desc)
 
 	def update(self):
 		self._time = datetime.datetime.utcnow()
@@ -227,13 +119,12 @@ class Bbc(Source):
 		soup = BeautifulSoup(html, "html.parser")
 		links = soup.find_all('div', {'class':'gs-c-promo'})
 		self.__isolate_content(links)
-		self._characterize()
 
 class Guardian(Source):
 	def __isolate_content(self, links):
 		self._content = []
 		for h in links:
-			desc = ''
+			desc = None
 			try:
 				title = h.contents[0]
 			except TypeError:
@@ -244,18 +135,7 @@ class Guardian(Source):
 			url = h['href']
 			if url.startswith('/'):
 				url = urljoin('https://www.theguardian.com', url)
-			machine_title = annotator.annotate(title)
-			thresh = 2
-			if desc:
-				thresh += 1
-			desc = annotator.annotate(desc)
-			self._content.append({
-				'title':title,
-				'machine_title':machine_title,
-				'description':desc,
-				'threshold':thresh,
-				'url':url
-			})
+			self._process(url, title, desc)
 
 	def update(self):
 		self._time = datetime.datetime.utcnow()
@@ -264,13 +144,12 @@ class Guardian(Source):
 		soup = BeautifulSoup(html, "html.parser")
 		links = soup.find_all('a', {'class':'js-headline-text'})
 		self.__isolate_content(links)
-		self._characterize()
 
 class Hill(Source):
 	def __isolate_content(self, links):
 		self._content = []
 		for h in links:
-			desc = ''
+			desc = None
 			title = ''
 			try:
 				if ('menu__link' not in h['class'] and 
@@ -291,18 +170,7 @@ class Hill(Source):
 				continue
 			if url.startswith('/'):
 				url = urljoin('http://thehill.com/', url)
-			machine_title = annotator.annotate(title)
-			thresh = 2
-			if desc:
-				thresh += 1
-			desc = annotator.annotate(desc)
-			self._content.append({
-				'title':title,
-				'machine_title':machine_title,
-				'description':desc,
-				'threshold':thresh,
-				'url':url
-			})
+			self._process(url, title, desc)
 
 	def update(self):
 		self._time = datetime.datetime.utcnow()
@@ -311,31 +179,19 @@ class Hill(Source):
 		soup = BeautifulSoup(html, "html.parser")
 		links = soup.find_all('a')
 		self.__isolate_content(links)
-		self._characterize()
 
 class Wapo(Source):
 	def __isolate_content(self, links):
 		self._content = []
 		for h in links:
-			desc = ''
+			desc = None
 			title = h.contents[0]
 			if 'blurb' in h.parent.parent.contents[1]['class']:
 				desc = h.parent.parent.contents[1].contents[0]
 			url = h['href']
 			if url.startswith('/'):
 				url = urljoin('http://www.washingtonpost.com', url)
-			machine_title = annotator.annotate(title)
-			thresh = 2
-			if desc:
-				thresh += 1
-			desc = annotator.annotate(desc)
-			self._content.append({
-				'title':title,
-				'machine_title':machine_title,
-				'description':desc,
-				'threshold':thresh,
-				'url':url
-			})
+			self._process(url, title, desc)
 
 	def update(self):
 		self._time = datetime.datetime.utcnow()
@@ -344,142 +200,6 @@ class Wapo(Source):
 		soup = BeautifulSoup(html, "html.parser")
 		links = soup.find_all('a', {'data-pb-field':'web_headline'})
 		self.__isolate_content(links)
-		self._characterize()
-
-def uin():
-	'''Function to take user input from terminal for testing purposes (dead)'''
-	s = input('story: ')
-	story = annotator.annotate(s)
-	return(story)
-
-def dedup_entities(entities):
-	'''Deduplicate traits idetified by the characterization system'''
-	et = entities['talley']
-	for t in et:
-		e = 0
-		while e < len(et[t])-1:
-			while et[t][e].lower() in ENTITIES:
-				if et[t][e+1].lower() in ENTITIES[et[t][e].lower()]:
-					et[t][e] += ' '+et[t][e+1]
-					del et[t][e+1]
-				else:
-					break
-			if t not in ['things', 'actions'] and et[t][e].lower() in POSITIONS:
-				if POSITIONS[et[t][e].lower()] in et['things']:
-					del et[t][e]
-			e += 1
-	entities['talley'] = et
-	return(entities)
-
-def calc_overlap(title, s):
-	'''Calculates the overlap in characteristics between stories'''
-	overlap = 0
-	for k in title['talley'].keys():
-		for i in title['talley'][k]:
-			if k == 'places':
-				overlap += (i in s['talley'][k] and len(i) > 2)*2
-			else:
-				overlap += i in s['talley'][k] and len(i) > 2
-	for k in title['mandate'].keys():
-		for i in title['mandate'][k]:
-			overlap -= (i in s['mandate'][k] and
-						i in s['talley']['people'] and
-						len(i) > 2)
-	return(overlap)
-
-def score_stories(s, wc, source_url, t_off):
-	'''Gets a list of high scoring stories and their scores'''
-	stories = []
-	opinions = []
-	for h in wc:
-		o = calc_overlap(h['character'], s)
-		thresh = h['threshold']
-		thresh += t_off
-		url = h['url']
-		if ('/opinion/' in url or
-			'/opinions/' in url or
-			'/blogs/' in url or
-			'/commentisfree/' in url or
-			'/posteverything/' in url):
-			if o > (thresh - 1):
-				opinions.append({
-					'title':h['title'],
-					'url':url,
-					'score': o
-				})
-		elif url != source_url:
-			if o > thresh:
-				stories.append({
-					'title':h['title'],
-					'url':url,
-					'score': o
-				})
-	return(stories, opinions)
-
-def title_clean(title):
-	'''Remove fancy directional quotation marks that might break things.
-	This may be unnessisary due to the use of Google Cloud Natural language rather than NLTK
-	'''
-	title = title.replace('‘', "'")
-	title = title.replace('’', "'")
-	title = title.replace('“', '"')
-	title = title.replace('”', '"')
-	return(title)
-
-def process(title, sources, url):
-	'''Get similar stories for a story'''
-	title = title_clean(title)
-	stories = []
-	opinions = []
-	thresh_off = 0
-	ann = annotator.annotate(title)
-	# The default r NEEDS to be passed or the function below becomes possessed
-	t = get_characteristics_from_list(ann, {
-		'talley':{
-			'places':[],
-			'people':[],
-			'organizations':[],
-			'things':[],
-			'actions':[]
-		},
-		'mandate':{
-			'speakers':[]
-		}
-	})
-	t = dedup_entities(t)
-	if ':' in title and len(t['mandate']['speakers']) == 0:
-		thresh_off += 1
-	for s in sources:
-		res = s.get()
-		out = score_stories(t, res, url, thresh_off)
-		stories += out[0]
-		opinions += out[1]
-	return(stories, opinions)
-
-def test_in_sites(url):
-	'''Checks if the input story is from an acceptable site.
-	For example this will not return true for:
-	https://entertainment.theonion.com/showrunner-disappointed-world-will-never-see-episode-wh-1826423126
-	But will return true for:
-	https://www.nytimes.com/2018/05/29/opinion/roseanne-canceled-abc-racist-tweets.html
-	'''
-	for s in SITES:
-		if s in url:
-			return(True)
-	return(False)
-
-def template_links(stories):
-	out = ''
-	urls = []
-	n = 0
-	for s in reversed(sorted(stories, key=lambda k: k['score'])):
-		if s['url'] not in urls:
-			n += 1
-			out += '{n}. [{title}]({url}) ({score})\n'.format(
-				n=str(n), title=s["title"], url=s["url"], score=str(s["score"])
-			)
-			urls.append(s['url'])
-	return(out)
 
 def get_story_title(url):
 	'''Remove the little trailing bits that websites add to article titles.
@@ -503,88 +223,13 @@ def get_story_title(url):
 		title = title.split('«')[0]
 	return(title)
 
-try:
-	sources = pickle.load(open('sources.db', 'rb'))
-except FileNotFoundError:
-	sources = {
-		AlJazeera(),
-		Bbc(),
-		Guardian(),
-		Hill(),
-		Wapo()
-	}
-except EOFError:
-	sources = {
-		AlJazeera(),
-		Bbc(),
-		Guardian(),
-		Hill(),
-		Wapo()
-	}
-for s in reddit.subreddit('worldnews').hot(limit = 30):
-	if test_in_sites(s.url):
-		title = get_story_title(s.url)
-		stories, opinions = process(title, sources, s.url)
-		temp = Template(TEMPLATE)
-		articles = template_links(stories)
-		editorials = template_links(opinions)
-		if articles or editorials:
-			print(title)
-			print(
-				temp.substitute(
-					sources=articles,
-					opinions=editorials,
-					writer='/u/michaelh115',
-					code='https://github.com/michardy/sources-bot'
-				)
-			)
-			#s.reply(
-			#	temp.substitute(
-			#		sources=articles,
-			#		opinions=editorials,
-			#		writer='/u/michaelh115',
-			#		code='https://github.com/michardy/sources-bot'
-			#	)
-			#)
-'''
-for mention in reddit.inbox.mentions():
-	b = mention.body
-	urlregex = re.compile(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)')
-	if b.startswith('+/u/sourcesbot'):
-		b = b.replace('+/u/sourcesbot ', '')
-		b = b.replace('+/u/sourcesbot', '')
-		if b == '':
-			pass
-			#mention.reply('Error: empty request')
-		else:
-			urls = urlregex.search(b)
-			if urls is not None:
-				q = get_story_title(urls.group(0))
-			else:
-				q = b
-			stories, opinions = process(q, sources, urls.group(0))
-			temp = Template(TEMPLATE)
-			articles = template_links(stories)
-			editorials = template_links(opinions)
-			if articles or editorials:
-				print(q)
-				print(
-					temp.substitute(
-						sources=articles,
-						opinions=editorials,
-						writer='/u/michaelh115',
-						code='https://github.com/michardy/sources-bot'
-					)
-				)
-				mention.reply(
-					temp.substitute(
-						sources=articles,
-						opinions=editorials,
-						writer='/u/michaelh115',
-						code='https://github.com/michardy/sources-bot'
-					)
-				)
-			else:
-				mention.reply('No matching articles were found')
-'''
-pickle.dump(sources, open('sources.db', 'wb'))
+sources = {
+	AlJazeera(),
+	Bbc(),
+	Guardian(),
+	Hill(),
+	Wapo()
+}
+
+for source in sources:
+	source.get()
