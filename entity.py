f4d2998d19aa3ed7ef597ba096a23564c0cf30f8
Michael Hardy <98.michael.hardy@gmail.com>
2018-08-27 00:38:39 -0700

    Fixing entity ids

--- entity.py
+++ entity.py
@@ -3,9 +3,8 @@ class Entity():
 		self.type = e.type
 		self.salience = e.salience
 		self.mention_type = m.type
+		self.id = None
 		for meta in e.metadata:
 			if meta == "mid":
 				self.id = e.metadata[meta] 
 				break
-			else:
-				self.id = None
