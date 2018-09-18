8845a8537dec27a35d1810d50b54dcd1d471efa1
Michael Hardy <98.michael.hardy@gmail.com>
2018-09-18 15:54:51 -0700

    Missing comma

--- server/custom_handler.py
+++ server/custom_handler.py
@@ -3,4 +3,4 @@ from tornado.web import RequestHandler
 class SourcesbotHandler(RequestHandler):
 	def write_error(self, status_code, **kwargs):
 		'''Custom error pages'''
-		self.render('error.html' code=status_code, reason=self._reason)
+		self.render('error.html', code=status_code, reason=self._reason)
